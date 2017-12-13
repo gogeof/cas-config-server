5fa7f1f8ae6ff25a083148a2a07544512a6d80c1
limiancai <miancai.li@gmail.com>
2017-12-13 13:54:46 +0800

    调整代码

--- src/main/java/org/example/gogeof/cas/config/CasConfigApplication.java
+++ src/main/java/org/example/gogeof/cas/config/CasConfigApplication.java
@@ -1,4 +1,4 @@
-package com.carl.auth.sso.config;
+package org.example.gogeof.cas.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class SsoConfigApplication {
-
+public class CasConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SsoConfigApplication.class, args);
+        SpringApplication.run(org.example.gogeof.cas.config.CasConfigApplication.class, args);
     }
 }
